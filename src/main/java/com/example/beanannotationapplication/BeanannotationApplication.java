fe4c5d544b0b239b1f303ce47108fe06aef01ca9
Priyu714 <priyankarahinj714@gmail.com>
2023-08-01 15:09:05 +0530

    first commit

---
+++ src/main/java/com/example/beanannotationapplication/BeanannotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanannotationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanannotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanannotationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/beanannotationapplication/service/UserService.java
@@ -0,0 +1,2 @@
+package com.example.beanannotationapplication.service;public class UserService {
+}
